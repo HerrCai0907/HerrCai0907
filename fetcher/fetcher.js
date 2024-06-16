e513fa26a688e8a034f8291c61799d721edeb5b4
Congcong Cai <congcongcai0907@163.com>
2024-06-16 12:40:35 +0800

    update

---
+++ fetcher/fetcher.js
@@ -0,0 +1,79 @@
+import { Octokit } from "@octokit/core";
+import { env } from "process";
+import languages from "linguist-languages";
+import { basename, extname } from "path";
+
+const ignoreLanguages = ["WebAssembly", "JSON"];
+
+function prepareQuery() {
+  const startDate = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+  return encodeURIComponent(`author:HerrCai0907 -repo:HerrCai0907/HerrCai0907 committer-date:>${startDate.toISOString()}`);
+}
+
+const result = {};
+
+function statistic(file, lines) {
+  const ext = extname(file);
+  (() => {
+    for (let lang of Object.keys(languages)) {
+      if (!languages[lang].color) continue;
+      if (languages[lang].filenames?.includes(basename(file))) {
+        result[lang] = result[lang] ?? 0;
+        result[lang] += lines;
+        console.log(`add '${lang}' ${lines} lines due to '${file}'`);
+        return;
+      }
+    }
+    for (let lang of Object.keys(languages)) {
+      if (!languages[lang].color) continue;
+      if (languages[lang].extensions?.includes(ext)) {
+        result[lang] = result[lang] ?? 0;
+        result[lang] += lines;
+        console.log(`add '${lang}' ${lines} lines due to '${file}'`);
+        return;
+      }
+    }
+  })();
+}
+
+export async function fetchFromGithub() {
+  const octokit = new Octokit({
+    auth: env["TOKEN"],
+  });
+
+  const request = `GET /search/commits?q=${prepareQuery()}`;
+
+  console.log(`fetch ${request}`);
+  const hashes = new Set();
+
+  for (let pageIndex = 0; ; pageIndex++) {
+    const {
+      data: { items },
+    } = await octokit.request(request, {
+      headers: { "X-GitHub-Api-Version": "2022-11-28" },
+      per_page: 100,
+      page: pageIndex,
+    });
+
+    if (items.length == 0) {
+      break;
+    }
+    await Promise.all(
+      items.map(async (item) => {
+        if (hashes.has(item.sha)) {
+          return;
+        }
+        hashes.add(item.sha);
+        console.log(`fetch ${item.url}`);
+        const { data } = await octokit.request(item.url);
+        data.files.forEach((file) => {
+          statistic(file.filename, file.changes);
+        });
+      })
+    );
+  }
+  for (let lang of ignoreLanguages) {
+    delete result[lang];
+  }
+  return result;
+}
